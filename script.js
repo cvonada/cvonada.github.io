d03ad2df714ec93739cdff29a9ceee788833faf6
Courtney Vonada <courtneyvonada@gmail.com>
2020-12-14 17:14:34 -0500

    Prints, text

--- script.js
+++ script.js
@@ -172,17 +172,18 @@ const voice02 = document.getElementById('voice02-audio');
 const voice03 = document.getElementById('voice03-audio');
 const voice04 = document.getElementById('voice04-audio');
 const voice05 = document.getElementById('voice05-audio');
+const voice06 = document.getElementById('voice06-audio');
 
 lisa.addEventListener('mousedown', function() {
-	if (game.scrollTop < 1500) {
+	if (game.scrollTop < 100) {
 		typeWriter(0);
 		var clickLisa = document.getElementById('text00').style.display='block';
 		voice01.play();
-	} else if (game.scrollTop > 1500 && game.scrollTop < 3000) {
+	} else if (game.scrollTop > 100 && game.scrollTop < 2000) {
 		typeWriter(1);
 		var clickLisa = document.getElementById('text01').style.display='block';
 		voice02.play();
-	} else if (game.scrollTop > 3000 && game.scrollTop < 4500) {
+	} else if (game.scrollTop > 2000 && game.scrollTop < 4500) {
 		typeWriter(2);
 		var clickLisa = document.getElementById('text02').style.display='block';
 		voice03.play();
@@ -236,3 +237,63 @@ function typeWriter(index) {
   	}, 1000);
   }
 }
+
+// Spirit Text
+let spirit= document.getElementById('idle');
+let spirit02= document.getElementById('idle02');
+
+spirit.addEventListener('mouseover', function() {
+	typeWriter(6);
+	var clickLisa = document.getElementById('text06').style.display='block';
+	spirit.src = 'images/spirt_away.gif';
+	voice06.play();
+}); 
+
+spirit02.addEventListener('mouseover', function() {
+	typeWriter(7);
+	var clickLisa = document.getElementById('text07').style.display='block';
+	spirit02.src = 'images/spirt_away.gif';
+	voice05.play();
+}); 
+
+// Foot Print Text
+let foot01= document.getElementById('print01');
+let foot02= document.getElementById('print02');
+let foot03= document.getElementById('print03');
+let foot04= document.getElementById('print04');
+
+foot01.addEventListener('mousedown', function() {
+	typeWriter(8);
+	var clickLisa = document.getElementById('text08').style.display='block';
+	voice01.play();
+}); 
+
+foot02.addEventListener('mousedown', function() {
+	typeWriter(9);
+	var clickLisa = document.getElementById('text09').style.display='block';
+	voice02.play();
+}); 
+
+foot03.addEventListener('mousedown', function() {
+	typeWriter(10);
+	var clickLisa = document.getElementById('text10').style.display='block';
+	voice03.play();
+}); 
+
+foot04.addEventListener('mousedown', function() {
+	typeWriter(11);
+	var clickLisa = document.getElementById('text11').style.display='block';
+	voice04.play();
+}); 
+
+//Mute Button
+var muteOn = document.getElementById("clickOn");
+var muteOff = document.getElementById("clickOff");
+
+muteOn.addEventListener('mousedown', function() {
+	bgAudio.muted = true;
+}); 
+
+muteOff.addEventListener('mousedown', function() {
+	bgAudio.muted = false;
+}); 
